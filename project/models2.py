7b959129a0e9f9c43b200b888785b08be439e44e
aboud20-meet <aboud20@meet.mit.edu>
2018-07-25 13:45:45 +0300

    finished front and back end for display journey

--- project/models2.py
+++ project/models2.py
@@ -9,14 +9,17 @@ class Journey(UserMixin, db.Model):
     __tablename__ = "journey"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
+    title          =  db.Column(db.String, nullable=False)
+    description    = db.Column(db.String, nullable=False)
     start_location = db.Column(db.String, nullable=False)
-    end_location = db.Column(db.String, nullable=False)
-    duration = db.Column(db.String, nullable=False)
-    category = db.Column(db.String, nullable=False)
-    requirements = db.Column(db.String, nullable=False)
-    people_range = db.Column(db.String, nullable=False)
+    end_location   = db.Column(db.String, nullable=False)
+    duration       = db.Column(db.String, nullable=False)
+    category       = db.Column(db.String, nullable=False)
+    requirements   = db.Column(db.String, nullable=False)
+    people_range   = db.Column(db.String, nullable=False)
+    price          = db.Column(db.String, nullable=False)
+    picture        = db.Column(db.String, nullable=False)
+    creator_id     = db.Column(dn.Integer, db.ForeignKey('users.id'), nullable=False)
 
 
     def __init__(self, email):
