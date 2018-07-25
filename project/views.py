0f3ccba45e1eda5d2fbb0d9a1167c03930fda472
aboud20-meet <aboud20@meet.mit.edu>
2018-07-25 11:12:52 +0300

    Please work

--- project/views.py
+++ project/views.py
@@ -3,15 +3,12 @@ from flask_login import login_required
 
 from . import app
 
+import os
+from twilio.rest import Client
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/private')
-@login_required
-def private_route():
-    return render_template('private.html')
 
 @app.route('/info')
 def info():
