ab48dd402713fe231e08c3b1b0df31569b3d013c
georgea18-meet <abudauodg@gmail.com>
2018-07-25 18:15:52 +0300

    just pulled

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -2,20 +2,22 @@ $(document).ready(function() {
     debugger;
 });
 
-var config = {
-    apiKey: "AIzaSyCMcif23MajRqImpvSj34QcE7Br47Q_Sbw",
-    authDomain: "easylicense-e9174.firebaseapp.com",
-    databaseURL: "https://easylicense-e9174.firebaseio.com",
-    projectId: "easylicense-e9174",
-    storageBucket: "easylicense-e9174.appspot.com",
-    messagingSenderId: "467829835580"};
+function start_firebase(){
+	var config = {
+	    apiKey: "AIzaSyCMcif23MajRqImpvSj34QcE7Br47Q_Sbw",
+	    authDomain: "easylicense-e9174.firebaseapp.com",
+	    databaseURL: "https://easylicense-e9174.firebaseio.com",
+	    projectId: "easylicense-e9174",
+	    storageBucket: "easylicense-e9174.appspot.com",
+	    messagingSenderId: "467829835580"};
 
-app = firebase.initializeApp(config);
-auth = firebase.auth(app);
-db = firebase.database(app);
-databaseRef = db.ref();
+	app = firebase.initializeApp(config);
+	auth = firebase.auth(app);
+	db = firebase.database(app);
+	databaseRef = db.ref();}
 
 function book(){
+	start_firebase();
 	var booking = {
     	name : document.forms["booking_form"]["name"].value,
     	phone_num : document.forms["booking_form"]["num"].value,
