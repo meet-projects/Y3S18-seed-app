18ac784e0d8d0a632b60daf6a13d3e19d984994a
ben18-meet <benlevi1905@gmail.com>
2018-07-26 11:10:50 +0300

    synax err

--- project/users.py
+++ project/users.py
@@ -27,15 +27,7 @@ def register():
                 db.session.add(user)
                 db.session.commit()
                 login_user(user, remember=True)
-<<<<<<< HEAD
-                next_page = request.args.get('next')
-                if not next_page or url_parse(next_page).netloc != '':
-                    next_page = url_for('private_route')
-                return redirect(next_page)
-            # else 
-=======
                 return url_for(feed(),  user=user)
->>>>>>> Post
         else:
             return Response("<p>invalid form</p>")
     return render_template('register.html', form=form)
