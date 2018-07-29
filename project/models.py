c272a5679cf5f6ad947026fc71ecd24a4d703190
Amir142 <amiiir14.2@gmail.com>
2018-07-29 12:06:14 +0300

    fixed date format 2.1

--- project/models.py
+++ project/models.py
@@ -77,7 +77,7 @@ class Post(db.Model):
         return str(now.month) + " - " + str(now.day) + " - " + str(now.year)
 
     def get_description(self):
-        return self.text[:100] + "..."
+        return self.Text[:100] + "..."
 
     def __repr__(self):
         return "post " + str(self.id) + " " + str(self.Title) + " " + str(self.Text)
