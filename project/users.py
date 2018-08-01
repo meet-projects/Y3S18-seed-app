5b17f4519ac004f69a87b50006a1373d5f772da3
May18-meet <may3825@gmail.com>
2018-08-01 22:51:18 +0300

    feeds css

--- project/users.py
+++ project/users.py
@@ -53,9 +53,13 @@ def login():
 			return redirect(url_for('feed'))
 		else:
 			return Response("<p>invalid login form</p>")
-	if current_user:
-		return redirect(url_for('feed'))
-	return render_template('login.html', form = form)
+
+	else:		
+		return render_template('login.html', form = form)
+
+	#if current_user:
+	#	return redirect(url_for('feed'))
+	#return render_template('login.html', form = form)
 
 @users_bp.route('/logout')
 @login_required
--- project/views.py
+++ project/views.py
@@ -18,16 +18,17 @@ def profile(username):
     visiting_user = User.query.filter_by(username=username)
     return render_template('profile.html', visiting_user=visiting_user)
 
-
+@app.route('/inspiration')
+@login_required
+def stories():
+    posts = Post.query.filter_by(ArtURL = '').all()
+    return render_template('stories.html', posts=posts)
 
 #read more function and add art
 @app.route('/stories/<int:post_id>', methods = ['GET','POST'])
 @login_required
 def list_detail_stories(post_id):
-    if post_id:
-        form = AddArtForm(request.form)
-        post = Post.query.filter_by(id = post_id).first()
-        return render_template('viewstory.html', post=post, form=form)
-    else:
-        posts = Post.query.filter_by(ArtURL = '').all()
-        return render_template('stories.html', posts=posts)
+    form = AddArtForm(request.form)    
+    post = Post.query.filter_by(id = post_id).first()
+    return render_template('viewstory.html', post=post, form=form)
+    
